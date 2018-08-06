4963465f765a4fc23ad03a4569cae1161fcff2cf
Patrick Vargas <pevargas90@gmail.com>
2018-08-05 20:44:47 -0400

    Add cli arguments to program

--- src/main/java/com/yesthisispatrick/games/minesweeper/App.java
+++ src/main/java/com/yesthisispatrick/games/minesweeper/App.java
@@ -4,7 +4,10 @@ import com.yesthisispatrick.games.minesweeper.Tile.TileFactory;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println(new Board().init().printBoard());
-    System.out.println(TileFactory.printStatistics());
+    Configuration config = new Configuration(args);
+    System.out.println(new Board(config).init().printBoard(config.isDebugMode()));
+    if (config.isDebugMode()) {
+      System.out.println(TileFactory.printStatistics());
+    }
   }
 }
